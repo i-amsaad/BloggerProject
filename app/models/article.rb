70826603c7929e22ad6fb8691c5550405e361fbb
Saad <muhammadsaad7891@gmail.com>
2019-07-02 22:28:45 -0700

    Image Uploaded

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,10 @@ class Article < ApplicationRecord
    has_many :tags,
    through: :taggings
 
+   has_attached_file :image, styles: { medium: "300x300>", thumb: "50x50>" }
+
+   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
 def tag_list
   self.tags.collect do |tag|
      tag.name
